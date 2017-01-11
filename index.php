4de487a172dccd1e2d44d8680e8893625cccb491
Joseph Ramirez <jramirez98@cnm.edu>
2017-01-10 22:32:24 -0700

    started new project. added list, column,

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Practice HTML</title>
+	</head>
+	<body>
+		<header>
+			<h1 class="foo-heading">This is a Demo Page</h1>
+		</header>
+		<main>
+			<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+
+			<h2>My Favorite Music</h2>
+			<ul>
+				<li>The Smiths</li>
+				<li>The Cure</li>
+				<li>Psychedelic Furs</li>
+				<li>The Stone Roses</li>
+				<li>Morrissey</li>
+				<li>Morrissey</li>
+			</ul>
+
+			<h3>this is a column</h3>
+			<table>
+				<tr>
+					<th>Domestic</th>
+					<th>Foreign</th>
+				</tr>
+				<tr>
+					<th>Ford</th>
+					<th>Toyota</th>
+				</tr>
+				<tr>
+					<th>Chevy</th>
+					<th>Nissan</th>
+				</tr>
+				<tr>
+					<th>Dodge</th>
+					<th>Subaru</th>
+				</tr>
+
+			</table>
+
+			<p>this is a link tag to <a href="https://google.com" target="_blank">Google</a></p>
+
+		</main>
+	</body>
+</html>
